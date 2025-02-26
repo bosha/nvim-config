83163df069924a1827dd98e92fe127c6526a87f2
Alex Bo <thebosha@gmail.com>
2025-02-26 15:07:44 +0400

    couple preferences for markdown-preview.nvim

--- lua/bosha/plugins/markdown.lua
+++ lua/bosha/plugins/markdown.lua
@@ -1,8 +1,22 @@
 return {
 	{
 		"iamcco/markdown-preview.nvim",
-		config = function()
+		build = function()
 			vim.fn["mkdp#util#install"]()
+		end,
+		config = function()
+			vim.cmd([[do FileType]])
+			vim.cmd([[
+				function OpenMarkdownPreview(url)
+					let cmd = "open -n -a 'Vivaldi' " . shellescape(a:url)
+					silent call system(cmd)
+				endfunction
+			]])
+			vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
+			vim.g.mkdp_auto_close = 0
+			vim.g.mkdp_preview_options = {
+				disable_sync_scroll = 1,
+			}
 		end,
 	},
 	{
