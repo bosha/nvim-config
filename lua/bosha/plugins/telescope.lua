e31bfbb5bf7efcc1d474f6c513b6f25630656e34
Alex Bo <thebosha@gmail.com>
2025-02-26 19:35:27 +0400

    additional keyboard shortcuts for git

--- lua/bosha/plugins/telescope.lua
+++ lua/bosha/plugins/telescope.lua
@@ -1,15 +1,20 @@
 return {
 	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		dependencies = { 'nvim-lua/plenary.nvim' },
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
-			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-			vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-			vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-			vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-			vim.keymap.set('n', '<leader>fc', builtin.commands, { desc = 'Telescope commands' })
-			vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+			vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "Telescope commands" })
+			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+
+			vim.keymap.set("n", "<leader>vb", builtin.git_branches, { desc = "[V]CS [B]ranches" })
+			vim.keymap.set("n", "<leader>vc", builtin.git_bcommits, { desc = "[V]CS buffer [c]commits" })
+			vim.keymap.set("n", "<leader>vc", builtin.git_commits, { desc = "[V]CS repo [C]ommits" })
 		end,
 	},
 	{
@@ -18,11 +23,10 @@ return {
 			local telescope = require("telescope")
 			telescope.setup({
 				["ui-select"] = {
-					require("telescope.themes").get_dropdown {
-					}
-				}
+					require("telescope.themes").get_dropdown({}),
+				},
 			})
 			require("telescope").load_extension("ui-select")
 		end,
-	}
+	},
 }
