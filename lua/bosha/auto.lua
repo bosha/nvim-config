3352619c47f230845291a9c425f447cb30e7fa4a
Alex Bo <thebosha@gmail.com>
2025-02-26 19:36:26 +0400

    automatic keyboard layout switch to US when enter normal mode

--- lua/bosha/auto.lua
+++ lua/bosha/auto.lua
@@ -6,7 +6,6 @@ vim.api.nvim_create_autocmd("VimEnter", {
 	end,
 })
 
-
 vim.api.nvim_create_autocmd("TermOpen", {
 	group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
 	callback = function()
@@ -14,3 +13,11 @@ vim.api.nvim_create_autocmd("TermOpen", {
 		vim.opt.relativenumber = false
 	end,
 })
+
+if vim.fn.executable("kbdswitch") == 1 then
+	vim.api.nvim_create_autocmd("InsertLeave", {
+		callback = function()
+			vim.api.nvim_command("silent !kbdswitch set com.apple.keylayout.ABC")
+		end,
+	})
+end
